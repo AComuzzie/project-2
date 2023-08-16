a937aa6100c019848ae77387198a362f1b273b29
Maren Christensen <marenjoychristensen@gmail.com>
2023-08-15 21:24:55 -0500

    fixed brakets within the connection.js file

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// This is where we create a user
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -15,6 +16,8 @@ router.post('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+
 
 router.post('/login', async (req, res) => {
   try {
--- models/Listing.js
+++ models/Listing.js
@@ -1,8 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
 class Listing extends Model {}
-
 Listing.init( //change to more relevant fields below
   {
     id: {
@@ -37,21 +35,27 @@ Listing.init( //change to more relevant fields below
         unique: true,
         validate: {
           isEmail: true,
-        },
+        }
+      },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'user',
         key: 'id',
-      },
-    },
+      }
+    }
   },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'listing',
-  },
+    modelName: 'event'
+  }
 );
-
 module.exports = Listing;
+
+
+
+
+
