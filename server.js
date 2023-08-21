69a26931f948d628b821d69957a208f16ddff904
Maren Christensen <marenjoychristensen@gmail.com>
2023-08-20 20:06:53 -0500

    add routes

--- server.js
+++ server.js
@@ -32,14 +32,20 @@ const sess = {
   
   app.engine('handlebars', hbs.engine);
   app.set('view engine', 'handlebars');
+
   
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.static(path.join(__dirname, 'public')));
   
+
+
   app.use(require('./controllers/'));
+
+  const jobboardRoutes = require('./controllers/jobboardRoutes');
+  app.use('/jobboard', jobboardRoutes);
   
-  // sequelize.sync({ force: false }).then(() => {
-  //   app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
-  // });
+  sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
+  });
   
